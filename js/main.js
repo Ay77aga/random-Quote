6baf14abe5039f69025877ef01bae36dbb1d5567
bassel444555@gmail.com <bassel444555@gmail.com>
2022-04-16 23:16:40 +0200

    add API

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
 let quote = document.querySelector('.quote'),
   say = document.querySelector('.say');
 // get data from json file
-const getson = fetch('./quotes.json')
+fetch('https://type.fit/api/quotes')
   .then(rep => rep.json())
   .then(function(data) {
+    console.log(data);
+
     // Get random number
     let random = Math.floor(Math.random() * (data.length - 1));
+
     // set quote in div
-    quote.innerHTML = data[random].quote;
-    say.textContent = data[random].say;
+    quote.innerHTML = data[random].text;
+    say.textContent = data[random].author;
+
     // randomly btn
     document.querySelector('button').onclick = function() {
+      quote.innerHTML = '';
       this.classList.remove('anim');
-      quote.classList.remove('op');
+      setTimeout(() => this.classList.add('anim'), 0);
       // make random number
       random = Math.floor(Math.random() * (data.length - 1));
-      quote.innerHTML = data[random].quote;
-      say.textContent = data[random].say;
-      // toggle animation
-      setTimeout(() => {
-        this.classList.add('anim');;
-        quote.classList.add('op');
-      }, 0);
+      quote.innerHTML = data[random].text;
+      say.textContent = data[random].author;
     }
   });
